14a126ddeeafd65ea919f9507213d0dd2222de93
Daniel Totten <TottenDC@gmail.com>
2019-02-10 11:09:03 -0500

    Adds library DB and creates initial view

--- app.js
+++ app.js
@@ -34,9 +34,15 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  // render appropriate error page
+  if (err.status === 404) {
+    res.status(err.status);
+    res.render('page-not-found');
+  } else {
+    res.status(err.status || 500);
+    res.render('error');
+  }
+  
 });
 
 module.exports = app;
